32e1a6c156780774d3baadb1ad5653803b3c35be
Matti Vuorre <mv2521@columbia.edu>
2017-07-12 15:11:32 -0400

    Allow custom variable names and multiple colored lines.

--- R/erp_scalp.R
+++ R/erp_scalp.R
@@ -2,59 +2,80 @@
 #'
 #' Creates an ERP figure for each channel and layouts them on the scalp.
 #'
-#' @param data An EEG dataset.
-#' @param electrode The column name containing electrode names in data.
+#' @param d An EEG dataset.
+#' @param electrode Column name containing electrode names in data.
+#' @param amplitude Column name containing amplitudes in data.
+#' @param time Column name containing time in data.
+#' @param color Variable to color lines by.
+#' @param size Size of line(s).
+#' @param show_guide Should a guide be shown.
 #'
+#' @author Matti Vuorre, \email{mv2521@columbia.edu}
 #' @import dplyr
+#' @import ggplot2
+#' @import tidyr
 #' @export
 
-erp_scalp <- function(data) {
+erp_scalp <- function(d,
+                      electrode = "electrode",
+                      amplitude = "amplitude",
+                      time = "time",
+                      color = "color",
+                      size = .7,
+                      show_guide = TRUE) {
 
   # Get default electrode locations from pkg internal data
-  data <- join_locs(data, .drop = TRUE)
+  d <- as.data.frame(d)
+  d <- join_locs(d, .drop = TRUE)
 
   # Data maxima for plot limits
-  maxAmp <- max(data$amplitude)
-  minAmp <- min(data$amplitude)
-  maxTime <- max(data$time)
-  minTime <- min(data$time)
+  maxAmp <- max(d[, amplitude])
+  minAmp <- min(d[, amplitude])
+  maxTime <- max(d[, time])
+  minTime <- min(d[, time])
 
   plotfun <- function(x) {
-    ggplot(data = x, aes(time, amplitude)) +
-      geom_line() +
-      facet_wrap("electrodelabel") +
-      coord_cartesian(ylim = c(minAmp, maxAmp),
-                      xlim = c(minTime, maxTime)) +
-      geom_vline(xintercept = 0, size=.3) +
-      geom_hline(yintercept = 0, size=.3) +
-      theme_void()
+    plot <- ggplot(data = x, aes_(as.name(time), as.name(amplitude)))
+    plot <- plot + facet_wrap("electrodefacet")
+    plot <- plot + coord_cartesian(ylim = c(minAmp, maxAmp),
+                                   xlim = c(minTime, maxTime))
+    plot <- plot + geom_vline(xintercept = 0, size=.3)
+    plot <- plot + geom_hline(yintercept = 0, size=.3)
+    plot <- plot + theme_void()
+    plot <- plot + scale_color_brewer(palette = "Set1")
+    if (!is.na(color)) {
+      plot <- plot + geom_line(aes_(color = as.name(color)),
+                               show.legend = FALSE, size = size)
+    } else {
+      plot <- plot + geom_line(size = size)
+    }
+    return(plot)
   }
 
-  d <- data %>%
-    mutate(electrodelabel = electrode) %>%
-    nest(-electrode) %>%
-    mutate(plot = map(data, plotfun)) %>%
-    select(-data) %>%
-    join_locs(.drop = T)
+  d$electrodefacet <- d[, electrode]
+  d <- nest(d, -electrode)
+  d <- mutate(d, plot = map(data, plotfun))
+  d <- select(d, -data)
+  d <- join_locs(d, .drop = T)
   d <- mutate(d, x = x + abs(min(x)), y = y + abs(min(y)))
 
   p <- ggplot(d, aes(x, y)) + theme_void()
-  p + geom_point() + geom_label(aes(label = electrode))
-  library(scales)
+
   guide <- ggplot(data, aes(x=time, y=amplitude)) +
     coord_cartesian(ylim = c(minAmp, maxAmp),
                     xlim = c(minTime, maxTime)) +
-    scale_x_continuous(breaks = pretty_breaks(n = 5)) +
-    scale_y_continuous(breaks = pretty_breaks(n = 4)) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 5)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +
     geom_vline(xintercept = 0) +
     geom_hline(yintercept = 0) +
-    theme_minimal() +
+    theme_minimal(base_size = 10) +
     theme(panel.grid = element_blank(),
           axis.ticks = element_line(size=.5))
-  guide
-  p <- p + annotation_custom(grob = ggplotGrob(guide),
-                             xmin = min(d$x)-.05, xmax = min(d$x)+.15,
-                             ymin = min(d$y)-.05, ymax = min(d$y)+.1)
+  if (show_guide) {
+    p <- p + annotation_custom(grob = ggplotGrob(guide),
+                               xmin = min(d$x)-.05, xmax = min(d$x)+.15,
+                               ymin = min(d$y)-.05, ymax = min(d$y)+.1)
+  }
   for (i in 1:59) {
     p <- p + annotation_custom(grob = ggplotGrob(d$plot[[i]]),
                                xmin = d$x[i]-.04, xmax = d$x[i]+.04,
