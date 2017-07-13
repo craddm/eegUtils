12bd668f6ea7f51325cfbfb5beb86d15ec503808
Matti Vuorre <mv2521@columbia.edu>
2017-07-13 12:29:24 -0400

    Improve erp_scalp plots.

--- R/erp_scalp.R
+++ R/erp_scalp.R
@@ -1,14 +1,20 @@
 #' Plot ERPs on the scalp
 #'
-#' Creates an ERP figure for each channel and layouts them on the scalp.
+#' Creates an ERP figure for each electrode and layouts them on the scalp.
 #'
 #' @param d An EEG dataset.
 #' @param electrode Column name containing electrode names in data.
+#' Defaults to "electrode".
 #' @param amplitude Column name containing amplitudes in data.
-#' @param time Column name containing time in data.
-#' @param color Variable to color lines by.
+#' Defaults to "amplitude".
+#' @param time Column name containing time in data. Defaults to "time".
+#' @param color Variable to color lines by. If no variable is passed, only
+#' one line is drawn for each electrode.
 #' @param size Size of line(s).
 #' @param show_guide Should a guide be shown.
+#'
+#' @details The function uses default electrode names and locations contained
+#' in the package.
 #'
 #' @author Matti Vuorre, \email{mv2521@columbia.edu}
 #' @import dplyr
@@ -20,13 +26,11 @@ erp_scalp <- function(d,
                       electrode = "electrode",
                       amplitude = "amplitude",
                       time = "time",
-                      color = "color",
-                      size = .7,
+                      color = NA,
+                      size = .65,
                       show_guide = TRUE) {
 
-  # Get default electrode locations from pkg internal data
   d <- as.data.frame(d)
-  d <- join_locs(d, .drop = TRUE)
 
   # Data maxima for plot limits
   maxAmp <- max(d[, amplitude])
@@ -42,8 +46,9 @@ erp_scalp <- function(d,
     plot <- plot + geom_vline(xintercept = 0, size=.3)
     plot <- plot + geom_hline(yintercept = 0, size=.3)
     plot <- plot + theme_void()
-    plot <- plot + scale_color_brewer(palette = "Set1")
+    plot <- plot + theme(strip.text = element_text(size=8))
     if (!is.na(color)) {
+      plot <- plot + scale_color_brewer(palette = "Set1")
       plot <- plot + geom_line(aes_(color = as.name(color)),
                                show.legend = FALSE, size = size)
     } else {
@@ -56,30 +61,44 @@ erp_scalp <- function(d,
   d <- nest(d, -electrode)
   d <- mutate(d, plot = map(data, plotfun))
   d <- select(d, -data)
-  d <- join_locs(d, .drop = T)
-  d <- mutate(d, x = x + abs(min(x)), y = y + abs(min(y)))
 
-  p <- ggplot(d, aes(x, y)) + theme_void()
+  # Get default electrode locations from pkg internal data
+  d <- join_locs(d, .drop = T)
+
+  minx <- min(d$x)
+  maxx <- max(d$x)
+  miny <- min(d$y)
+  maxy <- max(d$y)
+
+  d$x <- d$x + abs(minx)
+  d$y <- d$y + abs(miny)
+
+  p <- ggplot(d, aes(x, y)) +
+    geom_blank() +
+    theme_void() +
+    theme(plot.margin = unit(c(8,8,8,8), "pt"))
 
   guide <- ggplot(data, aes(x=time, y=amplitude)) +
     coord_cartesian(ylim = c(minAmp, maxAmp),
                     xlim = c(minTime, maxTime)) +
     scale_x_continuous(breaks = scales::pretty_breaks(n = 5)) +
     scale_y_continuous(breaks = scales::pretty_breaks(n = 4)) +
-    geom_vline(xintercept = 0) +
-    geom_hline(yintercept = 0) +
-    theme_minimal(base_size = 10) +
+    geom_vline(xintercept = 0, size = .4) +
+    geom_hline(yintercept = 0, size = .4) +
+    labs(y = expression(paste("Amplitude (", mu,"V)")),
+         x = "Time (ms)") +
+    theme_minimal(base_size = 8) +
     theme(panel.grid = element_blank(),
           axis.ticks = element_line(size=.5))
   if (show_guide) {
     p <- p + annotation_custom(grob = ggplotGrob(guide),
-                               xmin = min(d$x)-.05, xmax = min(d$x)+.15,
-                               ymin = min(d$y)-.05, ymax = min(d$y)+.1)
+                               xmin = min(d$x)-.05, xmax = min(d$x)+.125,
+                               ymin = min(d$y)-.05, ymax = min(d$y)+.125)
   }
-  for (i in 1:59) {
+  for (i in 1:nrow(d)) {
     p <- p + annotation_custom(grob = ggplotGrob(d$plot[[i]]),
-                               xmin = d$x[i]-.04, xmax = d$x[i]+.04,
-                               ymin = d$y[i]-.04, ymax = d$y[i]+.04)
+                               xmin = d$x[i]-.05, xmax = d$x[i]+.05,
+                               ymin = d$y[i]-.05, ymax = d$y[i]+.05)
   }
 
   return(p)
