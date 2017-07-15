49fe5f81f5f304e303c31e8dd919b36639403ac7
craddm <monklefish@gmail.com>
2017-07-15 11:58:09 +0100

    Simple interactive version of scalp_erps

---
+++ R/interactive_scalp.R
@@ -0,0 +1,65 @@
+#' Interactive scalp maps
+#'
+#' @author Matt Craddock, \email{m.p.craddock@leeds.ac.uk}
+#' @import shiny
+#' @import miniUI
+#' @export
+
+interactive_scalp <- function(data) {
+
+  ui <- miniPage(
+    gadgetTitleBar("Scalp ERPs"),
+    miniTabstripPanel(
+      miniTabPanel("Whole scalp", icon = icon("sliders"),
+        miniContentPanel(
+          fillCol(
+            flex = c(3,1),
+            plotOutput("Scalp", height = "100%",
+                       click = "click_plot"),
+            verbatimTextOutput("click_info")
+          )
+        )
+      ),
+      miniTabPanel("Selected electrodes", icon = icon("line-chart"),
+        miniContentPanel(
+          plotOutput("Selected", height = "100%")
+          )
+        )
+      )
+    )
+
+  server <- function(input, output, session) {
+
+    data <- electrode_locations(data)
+    elec_list <- reactiveValues(sel_elecs = NULL)
+
+    output$Scalp <- renderPlot({
+      erp_scalp(data)
+      })
+
+    observeEvent(input$click_plot, {
+      tmp <- nearPoints(data, input$click_plot, "x", "y", threshold = 45, maxpoints = 1)
+
+      if (tmp$electrode %in% elec_list) {
+        elec_list <- elec_list[-which(elec_list == tmp$electrode)]
+      } else{
+        elec_list <- c(elec_list,tmp$electrode)
+      }
+
+      output$click_info <- renderPrint({
+        paste("Selected:", elec_list)
+        })
+
+      output$Selected <- renderPlot({
+        plot_timecourse(data[data$electrode == tmp$electrode, ])
+      })
+    })
+
+    observeEvent(input$done, {
+      stopApp()
+    })
+
+  }
+
+  runGadget(ui, server)
+}
