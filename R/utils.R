f2a8abd13aade7614ea3acbf129c92d242a6b735
craddm <monklefish@gmail.com>
2017-07-15 12:48:15 +0100

    Added select_times helper function.

--- R/utils.R
+++ R/utils.R
@@ -36,3 +36,29 @@ electrode_locations <- function(data,
   }
 
 }
+
+#' Select timepoints from a given dataset
+#'
+#' @author Matt Craddock, \email{m.p.craddock@leeds.ac.uk}
+#'
+#' @param data An EEG dataset.
+#' @param time_lim A character vector of two numbers indicating the time range to be selected e.g. c(min, max)
+#' @return Data frame with only data from within the specified range.
+#'
+#' @export
+
+select_times <- function(data, time_lim) {
+
+  if ("time" %in% colnames(data)) {
+    if (length(time_lim) == 1) {
+      warning("Must enter two timepoints when selecting a time range; using whole range.")
+    } else if (length(time_lim) == 2) {
+      time_lim[1] <- data$time[which.min(abs(data$time - time_lim[1]))]
+      time_lim[2] <- data$time[which.min(abs(data$time - time_lim[2]))]
+      data <- filter(data, time >= time_lim[1] & time <= time_lim[2])
+    } else {
+      warning("No time column found.")
+    }
+  }
+  return(data)
+}
