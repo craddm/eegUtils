c1809dc16f4ef8ad2a684ea045c0753d0d706feb
craddm <monklefish@gmail.com>
2017-07-15 14:39:28 +0100

    Fixed plotting of invisible CIs

--- R/plot_timecourse.R
+++ R/plot_timecourse.R
@@ -5,10 +5,12 @@
 #'@author Matt Craddock, \email{m.p.craddock@leeds.ac.uk}
 #'
 #'@param data EEG dataset. Should have multiple timepoints.
+#'@param electrode Electrode(s) to plot.
 #'@param add_CI Add confidence intervals to the graph. Defaults to 95 percent between-subject CIs.
 #'@param time_lim Character vector. Numbers in whatever time unit is used specifying beginning and end of time-range to plot. e.g. c(-100,300)
 #'@param baseline Character vector. Times to use as a baseline. Takes the mean over the specified period and subtracts. e.g. c(-100,0)
-#'@param colour
+#'@param colour Variable to colour lines by. If no variable is passed, only
+#' one line is drawn for each electrode.
 #'@param facet Create multiple plots for a specified grouping variable.
 #'
 #'@import dplyr
@@ -16,23 +18,18 @@
 #'@return Returns a ggplot2 plot object
 #'@export
 
-plot_timecourse <- function(data, time_lim = NULL, group = NULL, facet = NULL, add_CI = FALSE, baseline = NULL, colour = NULL, color = NULL) {
+plot_timecourse <- function(data, time_lim = NULL, group = NULL, facet = NULL, add_CI = FALSE, baseline = NULL, colour = NULL, color = NULL, electrode = NULL) {
 
   # Filter out unwanted timepoints, and find nearest time values in the data --------------
 
-  if ("time" %in% colnames(data)) {
-    if (length(time_lim) == 1) {
-      warning("Must enter two timepoints when specifying a time range to plot; plotting whole range.")
-    } else if (length(time_lim) == 2) {
-      time_lim[1] <- data$time[which.min(abs(data$time - time_lim[1]))]
-      time_lim[2] <- data$time[which.min(abs(data$time - time_lim[2]))]
-      data <- filter(data, time >= time_lim[1] & time <= time_lim[2])
-    } else {
-    }
+  if (!is.null(time_lim)) {
+    data <- select_times(data, time_lim)
   }
 
+  # Average over all epochs in data (respecting "conditions"). --
   if ("epoch" %in% colnames(data)){
-    data <- summarise(group_by(data, time, electrode, condition), amplitude = mean(amplitude))
+    data <- summarise(group_by(data, time, electrode, condition),
+                      amplitude = mean(amplitude))
   }
 
   if (!is.null(group)) {
@@ -42,17 +39,25 @@ plot_timecourse <- function(data, time_lim = NULL, group = NULL, facet = NULL, a
   }
 
   ## Set up basic plot -----------
-  tc_plot <- ggplot(data, aes(x = time, y = amplitude))+
-    scale_color_brewer(palette = "Set1")
+  tc_plot <- ggplot(data, aes(x = time, y = amplitude))
 
-
-  tc_plot <- tc_plot +
-    stat_summary(fun.y = mean, geom = "line", size = 1)
+  if (!(is.null(colour) & is.null(color))) {
+    tc_plot <- tc_plot + scale_color_brewer(palette = "Set1")
+    tc_plot <- tc_plot +
+      stat_summary(fun.y = mean,
+                   geom = "line",
+                   aes_(colour = as.name(colour)),
+                   size = 1)
+  } else {
+    tc_plot <- tc_plot +
+      stat_summary(fun.y = mean,
+                   geom = "line",
+                   size = 1)
+  }
 
   if (!is.null(facet)) {
     if (facet %in% colnames(data)){
-      data <- data %>%
-        mutate_(f1 = facet)
+      data <- mutate_(data, f1 = facet)
       tc_plot <- tc_plot %+% data + facet_wrap(~f1)
     } else {
       warning("Unrecognised column name.")
@@ -62,18 +67,37 @@ plot_timecourse <- function(data, time_lim = NULL, group = NULL, facet = NULL, a
   ## Draw confidence intervals on plot.
 
   if (add_CI) {
+    if (!(is.null(colour) & is.null(color))){
     tc_plot <- tc_plot +
-      stat_summary(fun.data = mean_cl_normal, geom = "ribbon", linetype = "dotted", size = 1, alpha = 0.5)
+      stat_summary(fun.data = mean_cl_normal,
+                   geom = "ribbon",
+                   linetype = "dashed",
+                   aes_(colour = as.name(colour)),
+                   fill = NA,
+                   size = 1,
+                   alpha = 0.5)
+    } else {
+      tc_plot <- tc_plot +
+        stat_summary(fun.data = mean_cl_normal,
+                   geom = "ribbon",
+                   linetype = "dashed",
+                   fill = NA,
+                   size = 1,
+                   alpha = 0.5)
+    }
   }
 
-  tc_plot +
+  tc_plot <- tc_plot +
     labs(x = "Time (ms)", y = expression(paste("Amplitude (", mu, "V)")), colour = "", fill = "") +
-    geom_vline(xintercept = 0, linetype = "dashed", size = 1) +
-    geom_hline(yintercept = 0, linetype = "dashed", size = 1) +
-    scale_x_continuous(expand = c(0, 0)) +
-    theme_void() +
-    theme(strip.text = element_text(size=8))
+    geom_vline(xintercept = 0, linetype = "solid", size = 0.5) +
+    geom_hline(yintercept = 0, linetype = "solid", size = 0.5) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 4), expand = c(0,0)) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 3), expand = c(0,0)) +
+    theme_minimal(base_size = 12) +
+    theme(panel.grid = element_blank(),
+          axis.ticks = element_line(size=.5))
 
+  return(tc_plot)
 }
 
 #' Create a butterfly plot from timecourse data
@@ -91,15 +115,8 @@ plot_butterfly <- function(data, time_lim = NULL, group = NULL, facet = NULL, ba
 
   ## select time-range of interest -------------
 
-  if ("time" %in% colnames(data)) {
-    if (length(time_lim) == 1) {
-      warning("Must enter two timepoints when specifying a time range to plot; plotting whole range.")
-    } else if (length(time_lim) == 2) {
-      time_lim[1] <- data$time[which.min(abs(data$time - time_lim[1]))]
-      time_lim[2] <- data$time[which.min(abs(data$time - time_lim[2]))]
-      data <- filter(data, time >= time_lim[1] & time <= time_lim[2])
-    } else {
-    }
+  if (!is.null(time_lim)) {
+    data <- select_times(data, time_lim)
   }
 
   #Set up basic plot -----------
@@ -108,8 +125,10 @@ plot_butterfly <- function(data, time_lim = NULL, group = NULL, facet = NULL, ba
 
   butterfly_plot +
     labs(x = "Time (ms)", y = expression(paste("Amplitude (", mu, "V)")), colour = "") +
-    geom_vline(xintercept = 0, linetype = "dashed", size = 1) +
-    geom_hline(yintercept = 0, linetype = "dashed", size = 1) +
+    geom_vline(xintercept = 0, size = 0.5) +
+    geom_hline(yintercept = 0, size = 0.5) +
     scale_x_continuous(expand = c(0, 0)) +
-    theme_classic()
+    theme_minimal(base_size = 12) +
+    theme(panel.grid = element_blank(),
+          axis.ticks = element_line(size=.5))
 }
