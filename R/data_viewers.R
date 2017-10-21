afd14a909456953d3c02c1ecda33755790e94db5
craddm <monklefish@gmail.com>
2017-10-22 00:17:24 +0100

    Development on data viewer - mostly functioning with continuous data

--- R/data_viewers.R
+++ R/data_viewers.R
@@ -1,23 +1,36 @@
-#' Browse continuous data.
+#'Browse continuous data.
 #'
-#' @author Matt Craddock \email{m.p.craddock@leeds.ac.uk}
-#' @param df Data to be plotted. Must have columns headed time, amplitude, and electrode.
-#' @param sig_length Length of signal to be plotted initially (seconds if continuous, epochs if epoched).
-#' @param n_elecs Number of electrodes to be plotted on a single screen.
+#'A Shiny gadget for browsing continuous data interactively. Data can be viewed
+#'as a butterfly plot (all electrodes overlaid) or as individual traces
+#'(electrodes "stacked").
 #'
-#' @import tidyr
-#' @import dplyr
-#' @import ggforce
-#' @import ggplot2
-#' @import shiny
-#' @import miniUI
+#'@author Matt Craddock \email{matt@mattcraddock.com}
+#'
+#'@param df Data to be plotted. Must have columns headed time, amplitude, and
+#'  electrode.
+#'@param sig_length Length of signal to be plotted initially (seconds if
+#'  continuous, epochs if epoched).
+#'@param n_elecs Number of electrodes to be plotted on a single screen.
+#'@param downsample Only works on \code{eeg_data} objects. Reduces size of data
+#'  by only plotting every 4th point, speeding up plotting considerably.
+#'
+#'@import tidyr
+#'@import dplyr
+#'@import ggforce
+#'@import ggplot2
+#'@import shiny
+#'@import miniUI
 #'
 
-browse_data <- function(data, sig_length = 5, n_elecs = 20) {
+browse_data <- function(data, sig_length = 5, n_elecs = 20, downsample = FALSE) {
 
   if (is.eeg_data(data)) {
-    data <- cbind(data$signals, data$timings)
-    data <- gather(data, electrode, amplitude, -sample, -time)
+    #data <- cbind(data$signals, data$timings)
+    #data <- gather(data, electrode, amplitude, -sample, -time)
+    data <- as.data.frame(data, long = TRUE)
+    if (downsample) {
+      data <- data[seq(1, nrow(data), 4), ]
+    }
   }
 
   ui <- miniPage(
@@ -27,7 +40,7 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
                    miniContentPanel(
                      fillCol(
                        flex = c(4, NA, 1),
-                       plotOutput("butterfly", height = "100%"),
+                       plotOutput("butterfly", height = '100%'),
                        sliderInput("time_range",
                                    label = "Display start time",
                                    step = 1,
@@ -37,18 +50,23 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
                                    width = '100%'),
                        fillRow(
                          numericInput("sig_time", "Display length", sig_length, min = 1, max = 60),
-                         checkboxInput("dc_offset", "Remove DC offset", value = FALSE)
+                         numericInput("uV_scale", "Scale (microvolts)", max(data$signals), min = 10),
+                         checkboxInput("dc_offset", "Remove DC offset", value = TRUE)
                          )
                        )
                      )
                    ),
       miniTabPanel(title = "Individual" ,
                    miniContentPanel(
+                     wellPanel(
+                       plotOutput("time_plot"),
+                       style = "overflow-y:scroll; max-height: 800px"
+                     ),
                      fillPage(
-                       plotOutput("time_plot", height = 400),
                        fillCol(
-                         flex = c(NA, 1, 1),
-                         sliderInput("time_range",
+                         flex = c(NA, 2),
+
+                         sliderInput("time_range_ind",
                                      label = "Display start time",
                                      step = 1,
                                      min = 0,
@@ -56,12 +74,9 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
                                      value = min(unique(data$time)),
                                      width = '100%'),
                          fillRow(
-                           numericInput("sig_time", "Display length", sig_length, min = 1, max = 60),
-                           numericInput("elec_page", "Page", 1, min = 1, max = ceiling(length(unique(data$electrode))/n_elecs)),
-                           numericInput("elecs_per_page", "Electrodes per page", n_elecs, min = 1, max = 30)
-                           ),
-                         fillRow(
-                           checkboxInput("dc_offset", "Remove DC offset", value = FALSE)
+                           numericInput("sig_time_ind", "Display length", sig_length, min = 1, max = 60),
+                           numericInput("elecs_per_page_ind", "Electrodes per page", n_elecs, min = 1, max = 30),
+                           checkboxInput("dc_offset", "Remove DC offset", value = TRUE)
                            )
                          )
                        )
@@ -80,18 +95,20 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
       if (input$dc_offset) {
         tmp_data <- rm_baseline(tmp_data)
       }
-      plot_butterfly(tmp_data, legend = FALSE, continuous = TRUE)
+
+      zz <- plot_butterfly(tmp_data, legend = FALSE, continuous = TRUE)
+      zz
     })
 
     output$time_plot <- renderPlot({
       tmp_data <- dplyr::filter(data,
-                                time >= input$time_range,
-                                time <= (input$time_range + input$sig_time))
+                                time >= input$time_range_ind,
+                                time <= (input$time_range_ind + input$sig_time))
       if (input$dc_offset) {
         tmp_data <- rm_baseline(tmp_data)
       }
 
-      init_plot <- ggplot(tmp_data, aes(x = time, y = amplitude), environment = environment()) +
+      init_plot <- ggplot2::ggplot(tmp_data, aes(x = time, y = amplitude), environment = environment()) +
         geom_line() +
         facet_grid(electrode~., scales = "free_y", switch = "y") +
          # ggforce::facet_grid_paginate(electrode~.,
@@ -106,12 +123,15 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank(),
           axis.title.y = element_blank(),
-          strip.text.y = element_text(angle = 180)
+          strip.text.y = element_text(angle = 180),
+          panel.spacing = unit(0, "lines"),
+          panel.grid.minor = element_blank(),
+          panel.grid.major.y = element_blank()
         ) +
         scale_x_continuous(expand = c(0,0))
 
-      print(init_plot)
-    }, height = 800, inline = TRUE)
+      init_plot
+    }, height = 2000, inline = FALSE)
 
     observeEvent(input$done, {
       stopApp()
@@ -119,87 +139,4 @@ browse_data <- function(data, sig_length = 5, n_elecs = 20) {
     session$onSessionEnded(stopApp)
   }
   runGadget(ui, server, viewer = paneViewer(minHeight = 600))
-}
-
-
-#'
-#'
-#' @import tidyr
-#' @import dplyr
-#' @import ggforce
-#' @import ggplot2
-#' @import shiny
-#' @import miniUI
-#' @import plotly
-#'
-browse_data_shiny <- function(data, sig_length = 5, n_elecs = 20) {
-
-  if (is.eeg_data(data)) {
-    data <- cbind(data$signals, data$timings)
-    data <- gather(data, electrode, amplitude, -sample, -time)
-  }
-
-  shinyApp(
-    ui = fluidPage(
-      #gadgetTitleBar("Data browser"),
-      titlePanel("Data browser"),
-      #miniContentPanel(
-      mainPanel(
-        #fluidCol(
-          #flex = c(4, NA, 1, 1),
-          plotOutput("time_plot"),#, height = "100%"),
-          sliderInput("time_range",
-                      label = "Display start time",
-                      step = 1,
-                      min = 0,
-                      max = max(unique(data$time)),
-                      value = min(unique(data$time)),
-                      width = '100%'),
-          fluidRow(
-            numericInput("sig_time", "Display length", sig_length, min = 1, max = 60),
-            numericInput("elec_page", "Page", 1, min = 1, max = ceiling(length(unique(data$electrode))/n_elecs)),
-            numericInput("elecs_per_page", "Electrodes per page", n_elecs, min = 1, max = 30)
-          ),
-          fluidRow(
-            checkboxInput("dc_offset", "Remove DC offset", value = FALSE)
-          )
-        )
-      #)
-    ),
-
-    server = function(input, output, session) {
-      output$time_plot <- renderPlot({
-
-        tmp_data <- dplyr::filter(data, time >= input$time_range, time <= (input$time_range + input$sig_time))
-
-        if (input$dc_offset) {
-          tmp_data <- rm_baseline(tmp_data)
-        }
-
-        init_plot <- ggplot(tmp_data, aes(x = time, y = amplitude), environment = environment()) +
-          geom_line() +
-          ggforce::facet_grid_paginate(electrode~.,
-                                       scales = "free_y",
-                                       switch = "y",
-                                       ncol = 1,
-                                       nrow = input$elecs_per_page,
-                                       page = input$elec_page,
-                                       byrow = TRUE) +
-          theme_minimal() +
-          theme(
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            axis.title.y = element_blank(),
-            strip.text.y = element_text(angle = 180)
-          ) +
-          scale_x_continuous(expand = c(0,0))
-
-        print(init_plot)
-      })
-
-      observeEvent(input$done, {
-        stopApp()
-      })
-    }
-  )
 }
