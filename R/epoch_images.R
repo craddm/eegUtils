71ff3a1e29cee7589263e8e3cc2b2c2c67397e96
craddm <monklefish@gmail.com>
2018-03-11 15:59:52 +0000

    Added erp_raster() command

--- R/epoch_images.R
+++ R/epoch_images.R
@@ -1,12 +1,15 @@
 #' Plot ERP images
 #'
+#' Plot an ERP image from a single electrode. Smooths over a series of trials in
+#' order to make across-trial patterns more apparent.
+#'
 #' @param data Data frame to be plotted. Requires an amplitude column.
 #' @param electrode electrode at which to generate an ERP image.
 #' @param smoothing Number of trials to smooth over when generating image
 #' @param clim Character vector of min and max values of plotting colour range.
 #'   e.g. c(-5,5). Defaults to min and max.
 #'
-#' @author Matt Craddock, \email{matt@mattcraddock.com}
+#' @author Matt Craddock, \email{matt@@mattcraddock.com}
 #' @import ggplot2
 #' @import dplyr
 #' @importFrom scales squish
@@ -52,4 +55,105 @@ erp_image <- function(data, electrode = "Cz", smoothing = 10, clim = NULL) {
     scale_x_continuous(expand = c(0, 0)) +
     theme_classic() +
     xlab("Time (s)")
+}
+
+#' ERP raster plot
+#'
+#' Create a plot showing the ERP at every channel as a single ERP image. By
+#' default, this attempts to find channel locations and rearrange the channels
+#' such that spatial patterns on the scalp are more discernible. It orders the
+#' rows from the most posterior electrode on the right hemisphere to the most
+#' anterior electrode on the left hemisphere, with midline electrodes in the
+#' middle. If no locations are found, it simply displays the data in its
+#' original order.
+#'
+#' @param data An \code{eeg_epochs} object
+#' @param anat_order Arrange the channels in a more anatomically representative
+#'   order.
+#' @param time_lim Time limits of plot - should be a character vector (e.g.
+#'   c(-.2, .5))
+#' @param clim Character vector of min and max values of plotting colour range.
+#'   e.g. c(-5,5). Defaults to min and max.
+#' @import ggplot2
+#' @import dplyr
+#' @importFrom scales squish
+#' @author Matt Craddock, \email{matt@mattcraddock.com}
+#' @export
+
+erp_raster <- function(data, anat_order = TRUE, time_lim = NULL, clim = NULL) {
+
+  if (!is.null(time_lim)){
+    data <- select_times(data, time_lim)
+  }
+
+  if (all(anat_order && !is.null(data$chan_info))) {
+    chan_order <- arrange_chans(data)
+    data$signals <- data$signals[, chan_order]
+  }
+
+  data <- data.frame(data$signals, time = data$timings$time)
+  data <- dplyr::group_by(data, time)
+  data <- dplyr::summarise_all(data, mean)
+  data <- tidyr::gather(data, electrode, amplitude, -time, factor_key = TRUE)
+
+  if (is.null(clim)) {
+    clim <- c(min(data$amplitude), max(data$amplitude))
+  }
+
+  ggplot2::ggplot(data, aes(x = time, y = electrode, fill = amplitude)) +
+    geom_raster(interpolate = TRUE) +
+    geom_vline( xintercept = 0, linetype = "dashed", size = 2) +
+    scale_fill_distiller(palette = "RdBu",
+                         limits = c(clim[1], clim[2]),
+                         oob = scales::squish) +
+    scale_x_continuous(expand = c(0, 0)) +
+    theme_classic() +
+    labs(x = "Time (s)")
+}
+
+
+#' Topographical channel ordering
+#'
+#' Rearrange channels in a more anatomical order (e.g. left to right, anterior
+#' to posterior).
+#'
+#' @param data An \code{eeg_data} object
+#' @return Vector of channel positions
+#' @author Matt Craddock, \email{matt@mattcraddock.com}
+
+arrange_chans <- function(data) {
+
+  # Pick out electrodes on the midline (theta = 180 or 0)
+  midline <- data$chan_info$theta == 180 | data$chan_info$theta == 0
+  midline_labels <- data$chan_info$electrode[midline]
+  midline_dist <- ifelse(data$chan_info$theta[midline] == 180,
+                         -data$chan_info$radius[midline],
+                         data$chan_info$radius[midline])
+
+  # Order them from back to front
+  midline_labels <- midline_labels[sort(midline_dist,
+                                        decreasing = T,
+                                        index.return = T)$ix]
+
+  # Pick out electrodes from the left hemisphere
+  left <- sapply(data$chan_info$theta, function(x) x < 0 && x > -180)
+  left_labels <- data$chan_info$electrode[left]
+  left_dist <- data$chan_info$y[left]
+  left_labels <- left_labels[sort(left_dist,
+                                  index.return = T)$ix]
+
+  # Pirck out electrodes from the right hemisphere
+  right <- sapply(data$chan_info$theta, function(x) x > 0 && x < 180)
+  right_labels <- data$chan_info$electrode[right]
+  right_dist <- data$chan_info$y[right]
+  right_labels <- right_labels[sort(right_dist,
+                                    index.return = T)$ix]
+
+  all_labels <- c(left_labels, midline_labels, right_labels)
+  elecs_found <- toupper(names(data$signals)) %in% all_labels
+  elecs_not_found <- !elecs_found
+  new_ord <- match(toupper(c(all_labels,
+                             names(data$signals[!elecs_found]))),
+                   toupper(names(data$signals)))
+  new_ord
 }
