618744c04158f89b0bede01afb9a4ad359ed5119
craddm <monklefish@gmail.com>
2018-04-08 10:46:50 +0100

    added bdf import test

---
+++ tests/testthat/test-import.R
@@ -0,0 +1,17 @@
+context("Test import of bdf")
+
+test_data <- import_raw("Newtest17-256.bdf")
+
+test_that("Import of bdf files works correctly", {
+
+  skip_on_cran()
+
+  expect_s3_class(test_data, "eeg_data")
+  expect_true(is.eeg_data(test_data))
+  expect_false(is.eeg_epochs(test_data))
+  expect_true(is.data.frame(as.data.frame(test_data)))
+
+  test_epo <- epoch_data(test_data, 255)
+  expect_s3_class(test_epo, "eeg_epochs")
+
+})
