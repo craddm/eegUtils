3417e13e7f1efae1e7b1e650bf6bf27785d3fbe3
craddm <monklefish@gmail.com>
2018-07-04 10:10:21 +0100

    docs for the demo_epochs dataset

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' A demo \code{eeg_epochs} dataset
+#'
+#' This is a small dataset with 11 electrodes and 80 epochs used for
+#' demonstrating some features of the \code{eegUtils} package.
+#'
+#' @format A \code{eeg_epochs} object
+"demo_epochs"
