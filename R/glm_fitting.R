68a5c4be3d1e2511fb686fe1bd0bd6057779a643
craddm <monklefish@gmail.com>
2019-02-14 23:41:20 +0000

    glm fitting now works on eeg_epochs

--- R/glm_fitting.R
+++ R/glm_fitting.R
@@ -1,27 +1,44 @@
-#' Fit a glm model to each timepoint for an individual subject.
+#' Fit a GLM model to each timepoint for an individual subject.
+#'
+#' Fits a linear model to each timepoint using lm().
 #'
 #' @author Matt Craddock, \email{matt@@mattcraddock.com}
-#' @param data An EEG dataset.
 #' @param formula A regression formula for a GLM. See ?formula
+#' @param .data An \code{eegUtils} object.
+#' @param ... Any other arguments passed to (LM/GLM)
 #' @importFrom purrr map
 #' @importFrom tidyr nest
-#' @importFrom dplyr mutate ungroup is_grouped_df
-#' @noRd
+#' @importFrom dplyr mutate
+#' @export
 
-fit_glm_indiv <- function(data, formula) {
-  #options(contrasts=c('contr.sum','contr.poly'))
-  if (!"epoch" %in% colnames(data)) {
-    stop("Single trial data required for fitting.")
-  }
-  if (dplyr::is_grouped_df(data)) {
-    data <- dplyr::ungroup(data)
-  }
-  data <- tidyr::nest(data,
-                      condition,
-                      amplitude)
-  data <- dplyr::mutate(data,
-                        fit = purrr::map(data,
-                                         ~lm(formula,
-                                             data = .)))
+fit_glm <- function(formula,
+                    .data,
+                    ...) {
+  UseMethod("fit_glm", .data)
+}
 
+#' @export
+fit_glm.default <- function(formula,
+                            .data,
+                            ...) {
+  stop(paste("Objects of class", class(.data), "not currently supported"))
+
+}
+
+#' @export
+fit_glm.eeg_epochs <- function(formula,
+                               .data,
+                               ...) {
+
+  .data <- tibble::as_tibble(as.data.frame(.data,
+                                           long = TRUE,
+                                           coords = FALSE))
+  .data <- tidyr::nest(.data,
+                       -time,
+                       -electrode)
+  .data <- dplyr::mutate(.data,
+                         fit = purrr::map(data,
+                                          ~lm(formula,
+                                              data = .)))
+  .data
 }
