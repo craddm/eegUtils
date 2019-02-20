891e34a39112b71e33c11c085aa092f90a2142bc
craddm <monklefish@gmail.com>
2019-02-20 20:20:22 +0000

    joint diag from JADE now used instead of custom code

--- R/run_ICA.R
+++ R/run_ICA.R
@@ -34,7 +34,8 @@ run_ICA.eeg_epochs <- function(data,
   #}
 
   if (method == "sobi") {
-    ica_obj <- sobi_ICA(data)
+    ica_obj <- sobi_ICA(data,
+                        maxiter = maxit)
   } else {
 
     if (!requireNamespace("ica", quietly = TRUE)) {
@@ -85,11 +86,13 @@ run_ICA.eeg_epochs <- function(data,
 #' Internal function for running SOBI ICA on an \code{eeg_epochs} object
 #'
 #' @param data Data to be ICAed.
+#' @param maxiter Maximum number of iterations of the joint diagonalization
 #' @author A. Belouchrani and A. Cichocki. Adapted to R by Matt Craddock
 #'   \email{matt@@mattcraddock.com}
 #' @keywords internal
 
-sobi_ICA <- function(data) {
+sobi_ICA <- function(data,
+                     maxiter) {
 
   n_epochs <- length(unique(data$timings$epoch))
   n_channels <- ncol(data$signals)
@@ -104,17 +107,21 @@ sobi_ICA <- function(data) {
   #do by epochs
   amp_matrix <- split(data$signals, data$timings$epoch)
   amp_matrix <- lapply(amp_matrix,
-                       function(x) sweep(x,
-                                         2,
-                                         Matrix::colMeans(x))
-                       )
-  amp_matrix <- as.matrix(do.call("rbind", amp_matrix))
+                         function(x) sweep(x,
+                                           2,
+                                           Matrix::colMeans(x))
+                         )
+  # # amp_matrix <- lapply(amp_matrix,
+  # #                      function(x) scale(x, scale = FALSE))
+   amp_matrix <- as.matrix(do.call("rbind", amp_matrix))
+  #amp_matrix <- sweep(data$signals, 2, Matrix::colMeans(data$signals))
   SVD_amp <- svd(amp_matrix)
 
   ## get the psuedo-inverse of the diagonal matrix, multiply by singular
   ## vectors
-  Q <- MASS::ginv(diag(SVD_amp$d), tol = 0) %*% t(SVD_amp$v) # whitening matrix
-  amp_matrix <- Q %*% t(amp_matrix)
+   Q <- MASS::ginv(diag(SVD_amp$d), tol = 0) %*% t(SVD_amp$v) # whitening matrix
+   amp_matrix <- Q %*% t(amp_matrix)
+  #amp_matrix <- amp_matrix %*% SVD_amp$v
 
   ## reshape to reflect epoching structure
   dim(amp_matrix) <- c(n_channels,
@@ -142,18 +149,34 @@ sobi_ICA <- function(data) {
     Rxp <- Reduce("+",
                   Rxp)
     Rxp <- Rxp / n_epochs
-    M[, u:(u + n_channels - 1)] <- norm(Rxp, "F") * (Rxp) #  Frobenius norm
+    M[, u:(u + n_channels - 1)] <- norm(Rxp, "F") * (Rxp)
   }
 
   epsil <- 1 / sqrt(N) / 100
-  V <- joint_diag(M,
-                  epsil,
-                  n_channels,
-                  pm) # V = sphered unmixing matrix
+  # V <- joint_diag(M,
+  #                 epsil,
+  #                 n_channels,
+  #                 pm) # V = sphered unmixing matrix
+
+  V <- JADE::rjd(t(M),
+                 eps = epsil,
+                 maxiter = maxiter)$V
 
   ## create mixing matrix for output
-  mixing_matrix <- MASS::ginv(Q, tol = 0) %*% V
-  unmixing_matrix <- t(V)
+  mixing_matrix <- MASS::ginv(Q, tol = 0) %*% V # EEG.icawinv
+
+  mixing_2 <- MASS::ginv(mixing_matrix, tol = 0) # EEG.icaweights
+  # rescale vecs
+  scaling <- sqrt(colMeans(mixing_matrix^2))
+
+  weights <- sweep(mixing_2, MARGIN = 1, scaling, `*`) # scaled weights
+
+  icawinv <- MASS::ginv(weights %*% diag(ncol(weights)), tol = 0)#scaled EEG.icawinv
+
+
+  #mixing_matrix <- MASS::ginv(mixing_matrix, tol = 0)
+  #mixing_matrix <- SVD_amp$v %*% V
+  unmixing_matrix <- weights #t(V)
 
   dim(amp_matrix) <- c(n_channels,
                        n_times * n_epochs)
@@ -162,7 +185,7 @@ sobi_ICA <- function(data) {
   S <- as.data.frame(t(S))
   names(S) <- paste0("Comp", 1:ncol(S))
 
-  mixing_matrix <- as.data.frame(mixing_matrix)
+  mixing_matrix <- as.data.frame(icawinv)
   names(mixing_matrix) <- paste0("Comp", 1:ncol(S))
   mixing_matrix$electrode <- names(data$signals)
   unmixing_matrix <- as.data.frame(unmixing_matrix)
@@ -180,73 +203,6 @@ sobi_ICA <- function(data) {
   ica_obj
 }
 
-#' Joint diagonalization for SOBI
-#'
-#' Drop-in replacement for JADE::rjd, which often fails to converge
-#'
-#' @param M Numeric matrix
-#' @param eps Convergence tolerance
-#' @param pm number of channels * number of timepoints
-#' @param n_channels number of channels
-#' @author A. Belouchrani and A. Cichocki. Adapted to R by Matt Craddock
-#'   \email{matt@@mattcraddock.com}
-#' @keywords internal
-
-joint_diag <- function(M,
-                       eps,
-                       n_channels,
-                       pm) {
-
-  epsil <- eps
-  continue <- TRUE
-  V <- diag(n_channels)
-  step_n <- 0
-
-  while (isTRUE(continue)) {
-    continue <- FALSE
-    for (p in 1:(n_channels - 1)) {
-      for (q in ((p + 1):n_channels)) {
-
-        P_seq <- seq(p, pm, n_channels)
-        Q_seq <- seq(q, pm, n_channels)
-
-        # Perform Givens rotation
-        g <- rbind(M[p, P_seq] - M[q, Q_seq],
-                   M[p, Q_seq] + M[q, P_seq],
-                   1i * (M[q, P_seq] - M[p, Q_seq]))
-        eigs <- eigen(Re(tcrossprod(g))) # %*% t(g)))
-        sort_eigs <- sort(eigs$`values`, index.return = T)
-        angles <- eigs$vectors[, sort_eigs$ix[3]]
-        angles <- sign(angles[1]) * angles
-        c_r <- sqrt(0.5 + angles[1] / 2)
-        sr <- Re(0.5 * (angles[2] - 1i * angles[3]) / c_r)
-        sc <- Conj(sr)
-        conv_check <- abs(sr) > epsil
-        continue <- (continue | conv_check)
-        if (conv_check) {# Update the M and V matrices
-          colp <- M[, P_seq]
-          colq <- M[, Q_seq]
-          M[, P_seq] <- c_r * colp + sr * colq
-          M[, Q_seq] <- c_r * colq - sc * colp
-          rowp <- M[p, ]
-          rowq <- M[q, ]
-          M[p, ] <- c_r * rowp + sc * rowq
-          M[q, ] <- c_r * rowq - sr * rowp
-          temp <- V[, p]
-          V[, p] <- c_r * V[, p] + sr * V[, q]
-          V[, q] <- c_r * V[, q] - sc * temp
-        }
-      }
-    }
-    step_n <- step_n + 1
-    message("Iteration = ", step_n, "\n")
-  }
-  V
-}
-
-
-#
-
 apply_ica <- function(data, ...) {
   UseMethod("apply_ica", data)
 }
@@ -260,12 +216,12 @@ apply_ica.eeg_ICA <- function(data, comps, ...) {
   names(new_dat) <- data$chan_info$electrode
   out <-
     eeg_data(new_dat,
-           data$srate,
-           data$events,
-           data$chan_info,
-           data$timings,
-           data$continuous,
-           reference = NULL
+             data$srate,
+             data$events,
+             data$chan_info,
+             data$timings,
+             data$continuous,
+             reference = NULL
           )
   class(out) <- c("eeg_epochs", "eeg_data")
   out
@@ -279,7 +235,7 @@ apply_ica.eeg_epochs <- function(data, decomp, comps, ...) {
     new_mixmat <- decomp$mixing_matrix[1:(ncomps)]
     new_mixmat[, comps] <- 0
     source_sigs <- as.matrix(data$signals) %*%
-      as.matrix(decomp$unmixing_matrix[, 1:ncomps])
+      t(as.matrix(decomp$unmixing_matrix[, 1:ncomps]))
 
     new_dat <- as.matrix(new_mixmat) %*% t(source_sigs)
     new_dat <- as.data.frame(t(new_dat))
