b2d2ad4e2c21d3082b73d7baed778dd1211aad01
Matt Craddock <monklefish@gmail.com>
2020-01-19 11:21:11 +0000

    major glm updates continuing

--- R/glm_fitting.R
+++ R/glm_fitting.R
@@ -1,9 +1,21 @@
-#' Fit a GLM model to each timepoint for an individual subject
+#' Fit a linear model to EEG data
 #'
 #' Fits a linear model to each timepoint separately for each electrode.
 #'
+#' @section Notes On Usage:
+#'
+#'   The \code{fit_glm} function will fit a linear model to each timepoint for
+#'   each electrode in the input dataset.
+#'
+#'   The formula is a standard R formula. Specify only the right-hand side of
+#'   the formula i.e. any predictors.
+#'
+#'   The function allows flexible fitting of a baseline covariate, recognising
+#'   the term \code{baseline} in the specified formula.
+#'
 #' @author Matt Craddock, \email{matt@@mattcraddock.com}
-#' @param formula A regression formula for a GLM. See ?formula
+#' @param formula An object of class "\code{formula}". Right-hand side A regression formula for a GLM. See ?formula
+#'   and notes on use below
 #' @param data An \code{eegUtils} object.
 #' @param ... Any other arguments passed to (LM/GLM)
 #' @importFrom purrr map
@@ -17,7 +29,6 @@ fit_glm <- function(formula,
   UseMethod("fit_glm", data)
 }
 
-#' @export
 fit_glm.default <- function(formula,
                             data,
                             ...) {
@@ -25,6 +36,9 @@ fit_glm.default <- function(formula,
 
 }
 
+#' @param time_lim Numeric vector of length 2 specifying time period to be used
+#'   as a baseline
+#' @describeIn fit_glm GLM fitting for \code{eeg_epochs}
 #' @export
 fit_glm.eeg_epochs <- function(formula,
                                data,
@@ -56,72 +70,101 @@ fit_glm.eeg_epochs <- function(formula,
     mdf <- stats::model.matrix(formula,
                                mutate(epochs(data),
                                       baseline = 0))
-    #create one design matrix for each channel with the appropriate baseline for
-    #each epoch
+    n_preds <- ncol(mdf)
+    # create one design matrix for each channel with the appropriate baseline
+    # for each epoch
     mod_mats <- lapply(seq(1, n_chans),
                        function(x) model.matrix(formula,
                                                 mutate(epochs(data),
                                                        baseline = base_times[, x])))
     std_errs <- lapply(mod_mats,
-                       function(x) solve(crossprod(x)))
-
+                       function(x) chol2inv(qr(x)$qr[1:n_preds, 1:n_preds]))
+    #chol2inv(qr(mdf2)$qr[1:3, 1:3])
     out <- vector(mode = "list",
                   length = length(zip))
 
     out_se <- vector(mode = "list",
                      length = length(zip))
+    out_res <- vector(mode = "list",
+                     length = length(zip))
     tmp <- vector(mode = "list",
                   length = n_chans)
     tmp_se <- vector(mode = "list",
                      length = n_chans)
+    tmp_res <- vector(mode = "list",
+                      length = n_chans)
     n <- dim(mod_mats[[1]])[1]
     k <- dim(mod_mats[[1]])[2]
     resid_df <- n - k
 
+    tot_vars <- lapply(zip,
+                       function(x) colSums(scale(x,
+                                                 scale = FALSE)^2))
     # TODO add r_sq with baseline
 
     for (n_epo in (seq(1, length(zip)))) {
       for (i in (seq(1, n_chans))) {
-        tmp_mod <- .lm.fit(mod_mats[[i]], zip[[n_epo]][[i]])
+        tmp_mod <- .lm.fit(mod_mats[[i]],
+                           zip[[n_epo]][[i]])
         tmp[[i]] <- coef(tmp_mod)
-        tmp_se[[i]] <- sqrt(diag(std_errs[[i]] * as.numeric(crossprod(resid(tmp_mod))/(resid_df))))
+        tmp_res[[i]] <- sum(resid(tmp_mod)^2)
+        tmp_se[[i]] <- sqrt(diag(std_errs[[i]] * (tmp_res[[i]]/resid_df)))
       }
-      out[[n_epo]] <- do.call(cbind, tmp)
-      out_se[[n_epo]] <- do.call(cbind, tmp_se)
+      out[[n_epo]] <- do.call(cbind,
+                              tmp)
+      out_se[[n_epo]] <- do.call(cbind,
+                                 tmp_se)
+      out_res[[n_epo]] <- do.call(cbind,
+                                  tmp_res)
     }
+
+    r_sq <- tibble::as_tibble(1 - do.call(rbind,
+                                          out_res) / do.call(rbind,
+                                                             tot_vars))
+    r_sq$time <- unique(data$timings$time)
+
   } else {
 
     mdf <- stats::model.matrix(formula,
                                epochs(data))
-    std_errs <- solve(crossprod(mdf))
+    #std_errs <- solve(crossprod(mdf))
+    n_preds <- ncol(mdf)
+    #invert matrix using qr decomposition
+    std_errs <- chol2inv(qr(mdf)$qr[1:n_preds, 1:n_preds])
     resid_df <- nrow(mdf) - ncol(mdf)
-    tot_vars <- lapply(zip, function(x) colSums(scale(x, scale = FALSE)^2))
+    tot_vars <- lapply(zip,
+                       function(x) colSums(scale(x,
+                                                 scale = FALSE)^2))
 
-    new_t <- function(mdf, x, std_errs) {
-      tmp_mod <- .lm.fit(mdf, x)
-
-      ses <-  apply(resid(tmp_mod),
-                    2,
-                    crossprod) #/ resid_df
-      res_vars <- colSums(resid(tmp_mod)^2)
-
-      out_se <- lapply(ses,
-                       function(x) sqrt(diag(std_errs * (x/resid_df))))
-      list("coefs" = coef(tmp_mod),
-           "ses" = do.call(cbind,
-                           out_se),
-           "res_vars" = res_vars)
-    }
-
+    # now_t <- function(mdf, x, std_errs) {
+    #   tmp_mod <- .lm.fit(mdf, x)
+    #
+    #   # ses <-  apply(resid(tmp_mod),
+    #   #               2,
+    #   #               crossprod) #/ resid_df
+    #   res_vars <- resid(tmp_mod)^2
+    #
+    #   XDXs <- apply(res_vars,
+    #                 2,
+    #                 function(x) diag(rob_sd(mdf, x, std_errs)))
+    #
+    #   out_se <- sqrt(XDXs)
+    #   list("coefs" = coef(tmp_mod),
+    #        "ses" = out_se,
+    #        "res_vars" = colSums(res_vars))
+    # }
     out <- lapply(zip,
                   function(x) new_t(mdf,
                                     as.matrix(x),
-                                    std_errs))
+                                    std_errs,
+                                    resid_df))
     out_se <- lapply(out,
                      function(x) x$ses)
     res_vars <- lapply(out,
                        function(x) x$res_vars)
-    r_sq <- tibble::as_tibble(1 - do.call(rbind, res_vars) / do.call(rbind, tot_vars))
+    r_sq <- tibble::as_tibble(1 - do.call(rbind,
+                                          res_vars) / do.call(rbind,
+                                                              tot_vars))
     out <- lapply(out,
                   function(x) x$coefs)
     r_sq$time <- unique(data$timings$time)
@@ -149,4 +192,34 @@ fit_glm.eeg_epochs <- function(formula,
        "t_stats" = t_stats,
        "chan_info" = channels(data),
        "r_sq" = r_sq)
+}
+
+#' @param mdf model matrix
+#' @param x data to be modelled (matrix of trials x channels)
+#' @param std_errs
+new_t <- function(mdf,
+                  x,
+                  std_errs,
+                  resid_df) {
+
+  tmp_mod <- .lm.fit(mdf,
+                     x)
+
+  res_vars <- colSums(resid(tmp_mod)^2)
+
+  out_se <- lapply(res_vars,
+                   function(x) sqrt(diag(std_errs * (x/resid_df))))
+  list("coefs" = coef(tmp_mod),
+       "ses" = do.call(cbind,
+                       out_se),
+       "res_vars" = res_vars)
+}
+
+get_hat <- function(mdf, std_errs) {
+  diag(mdf %*% std_errs %*% t(mdf))
+}
+
+rob_se <- function(mdf, u2, XX) {
+  ouchman <- sweep(mdf, 1, u2, "*")
+  XX %*% (t(ouchman) %*% mdf) %*% XX
 }
