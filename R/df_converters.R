8bf5710bf3ef7718644e8c919196e6b4263c5fa8
Matt Craddock <monklefish@gmail.com>
2020-02-12 22:49:26 +0000

    working on df_converters separate file

--- R/df_converters.R
+++ R/df_converters.R
@@ -1,3 +1,86 @@
+#' Convert \code{eeg_tfr} objects to data frames
+#'
+#' @author Matt Craddock \email{matt@@mattcraddock.com}
+#' @param x Object of class \code{eeg_tfr}
+#' @param row.names Kept for compatability with S3 generic, ignored.
+#' @param optional Kept for compatability with S3 generic, ignored.
+#' @param long Convert to long format. Defaults to FALSE.
+#' @param ... arguments for other as.data.frame commands
+#'
+#' @importFrom tidyr spread
+#' @export
+as.data.frame.eeg_tfr <- function(x,
+                                  row.names = NULL,
+                                  optional = FALSE,
+                                  long = FALSE,
+                                  ...) {
+
+
+  if (length(dim(x$signals)) == 3) {
+    out_df <- aperm(x$signals,
+                    c("time",
+                      "frequency",
+                      "electrode"))
+    dim_size <- dim(out_df)
+    dim(out_df) <- c(dim_size[1] * dim_size[2],
+                     dim_size[3])
+    out_df <- as.data.frame(out_df)
+    names(out_df) <- dimnames(x$signals)$electrode
+    out_df$time <- rep(as.numeric(dimnames(x$signals)$time),
+                       dim_size[2])
+    out_df$frequency <- rep(as.numeric(dimnames(x$signals)$frequency),
+                            each = dim_size[1])
+    out_df$epoch <- unique(x$epochs$epoch)
+  } else {
+    # Check which dimenstion to average over - participant or epoch
+    if ("epoch" %in% x$dimensions) {
+      avg_dim <- "epoch"
+    } else if ("participant_id" %in% x$dimensions) {
+      avg_dim <- "participant_id"
+    }
+    out_df <- aperm(x$signals, c("time",
+                                 "frequency",
+                                 avg_dim,
+                                 "electrode"))
+    dim_size <- dim(out_df)
+    dim(out_df) <- c(dim_size[1] * dim_size[2] * dim_size[3],
+                     dim_size[4])
+    out_df <- as.data.frame(out_df)
+    names(out_df) <- dimnames(x$signals)$electrode
+    out_df$time <- rep(as.numeric(dimnames(x$signals)$time),
+                       dim_size[3] * dim_size[2])#out_df$time)
+    out_df$frequency <- rep(as.numeric(dimnames(x$signals)$frequency),
+                            each = dim_size[1])
+    out_df$epoch <- rep(x$epochs$epoch, each =
+                          dim_size[1] * dim_size[2])
+    out_df$participant_id <- rep(x$epochs$participant_id,
+                                 each = dim_size[1] * dim_size[2])
+
+
+  }
+
+  if ("participant_id" %in% x$dimensions) {
+    out_df <- dplyr::left_join(out_df,
+                               x$epochs,
+                               by = c("participant_id", "epoch"))
+  } else {
+    #out_df$epoch <- as.numeric(out_df$epoch)
+    out_df <- dplyr::left_join(out_df,
+                               x$epochs,
+                               by = "epoch")
+  }
+  if (long) {
+    return(tidyr::gather(out_df,
+                         electrode,
+                         power,
+                         channel_names(x)))
+  }
+
+  out_df
+}
+
+
+
 #' Convert `eeg_lm` to data.frame
 #'
 #' Convert an object of class \code{eeg_data} into a standard `data.frame`.
