c2bcd64fe7c1796029bb394d55aad0df0192e1a9
craddm <monklefish@gmail.com>
2021-07-15 13:44:30 +0100

    document new demo_spatial

--- R/data.R
+++ R/data.R
@@ -3,5 +3,14 @@
 #' This is a small dataset with 11 electrodes and 80 epochs used for
 #' demonstrating some features of the `eegUtils` package.
 #'
-#' @format A `eeg_epochs` object
+#' @format An `eeg_epochs` object
 "demo_epochs"
+
+#' A demo `eeg_epochs` dataset
+#'
+#' This is a small dataset with 23 electrodes and 80 epochs used for
+#' demonstrating some features of the `eegUtils` package. It is part of a
+#' recording from a visual spatial cueing task.
+#'
+#' @format An `eeg_epochs` object
+"demo_spatial"
