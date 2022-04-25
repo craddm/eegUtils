28a12b7e8ba8276e09997b9c5c2a55187c574be7
craddm <monklefish@gmail.com>
2022-04-25 22:53:09 +0100

    erp image tests for eeg_ica

--- tests/testthat/test-epoch_images.R
+++ tests/testthat/test-epoch_images.R
@@ -1,9 +1,14 @@
+demo_sobi <- run_ICA(demo_epochs, pca = 10)
 test_that("epoch images plot correctly", {
   skip_on_ci()
   vdiffr::expect_doppelganger(
     "ERP image A29 from demo epochs",
     erp_image(demo_epochs,
               "A29"))
+  vdiffr::expect_doppelganger(
+    "ERP image Comp001 from demo epochs",
+    erp_image(demo_sobi,
+              "Comp001"))
   vdiffr::expect_doppelganger(
     "ERP raster with facets",
     erp_raster(demo_spatial) + facet_wrap(~epoch_labels)
