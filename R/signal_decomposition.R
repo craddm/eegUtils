c86e65a919b03a326e1abfa6fb20cfa4c48a22bc
craddm <monklefish@gmail.com>
2023-09-13 16:43:57 +0100

    fix documentation errors and clarify options for decompositions

--- R/signal_decomposition.R
+++ R/signal_decomposition.R
@@ -2,19 +2,19 @@
 #'
 #' Implements a selection of Generalized Eigenvalue based decomposition methods
 #' for EEG signals. Intended for isolating oscillations at specified
-#' frequencies, decomposing channel-based data into components
-#' reflecting distinct or combinations of sources of oscillatory signals.
-#' Currently, spatio-spectral decomposition (Nikulin, Nolte, & Curio, 2011) and
-#' Rhythmic Entrainment Source Separation (Cohen & Gulbinate, 2017) are
-#' implemented. The key difference between the two is that the former returns
-#' the results of the data-derived spatial filters applied to the
-#' bandpass-filtered "signal" data, whereas the latter returns the results of the
-#' filters applied to the original, broadband data.
+#' frequencies, decomposing channel-based data into components reflecting
+#' distinct or combinations of sources of oscillatory signals. Currently,
+#' spatio-spectral decomposition (Nikulin, Nolte, & Curio, 2011) and Rhythmic
+#' Entrainment Source Separation (Cohen & Gulbinate, 2017) are implemented. The
+#' key difference between the two is that the former returns the results of the
+#' data-derived spatial filters applied to the bandpass-filtered "signal" data,
+#' whereas the latter returns the results of the filters applied to the
+#' original, broadband data.
 #'
-#' @param data An `eeg_data` object
+#' @param data An `eeg_epochs` object
 #' @param ... Additional parameters
 #' @author Matt Craddock \email{matt@@mattcraddock.com}
-#' @return An `eeg_ICA` object. Note that
+#' @return An `eeg_ICA` object.
 #' @examples
 #' # The default method is Spatio-Spectral Decomposition, which returns
 #' # spatially and temporally filtered source timecourses.
@@ -65,7 +65,7 @@ eeg_decompose.default <- function(data, ...) {
 
 #' @param sig_range Vector with two inputs, the lower and upper bounds of the frequency range of interest
 #' @param noise_range Range of frequencies to be considered noise (e.g. bounds of flanker frequencies)
-#' @param method Type of decomposition to apply. Currently only "ssd" is supported.
+#' @param method Type of decomposition to apply. Defaults to "ssd"
 #' @param verbose Informative messages printed to console. Defaults to TRUE.
 #' @param order Filter order for filter applied to signal/noise
 #' @describeIn eeg_decompose method for `eeg_epochs` objects
@@ -73,7 +73,7 @@ eeg_decompose.default <- function(data, ...) {
 eeg_decompose.eeg_epochs <- function(data,
                                      sig_range,
                                      noise_range,
-                                     method = "ssd",
+                                     method = c("ssd", "ress"),
                                      verbose = TRUE,
                                      order = 2,
                                      ...) {
@@ -82,7 +82,7 @@ eeg_decompose.eeg_epochs <- function(data,
     message("Performing ", method, "...")
   }
 
-  data <- switch(method,
+  data <- switch(match.arg(method),
                  "ssd" = run_SSD(data,
                                  sig_range,
                                  noise_range,
