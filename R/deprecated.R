01632f80a62adedd44f271d2fec3c4e368d62e3b
craddm <monklefish@gmail.com>
2023-10-18 22:31:26 +0100

    remove deprecated function

--- R/deprecated.R
+++ R/deprecated.R
@@ -5,12 +5,3 @@ eeg_FASTER <- function(data, ...) {
               msg = "Please use ar_FASTER; eeg_FASTER will be removed in a forthcoming release.")
   ar_FASTER(data, ...)
 }
-
-
-#'@rdname eeg_reference
-#'@export
-reref_eeg <- function(data, ...) {
-  .Deprecated("eeg_reference",
-              msg = "reref_eeg has been renamed eeg_reference, and will be removed in a future version of eegUtils.")
-  eeg_reference(data, ...)
-}
